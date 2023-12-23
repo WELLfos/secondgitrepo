a50ebf9a606c1c2ec50f68aba518c32a3918f62b
Сергей <kornyuhin.serg@yandex.ru>
2023-12-23 18:01:28 +0300

    2

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from PyQt5.QtCore import Qt
 import random
-from PyQt5 import uic
 import sys
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
+from Ui import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __int__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi()
         self.button_play.clicked.connect(self.create_yellow_circle)
-
-        self.radius = 0
+        self.radius = random.randint(50, 200)
 
     def create_yellow_circle(self):
         self.radius = random.randint(50, 200)
         self.update()
 
     def paintEvent(self, event):
+        self.radius = random.randint(50, 200)
         painter = QPainter(self)
         painter.setPen(Qt.NoPen)
-        painter.setBrush(QColor(255, 255, 0))
-        painter.drawEllipse(100, 100, 100, 100)
+        painter.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        painter.drawEllipse(100, 100, self.radius, self.radius)
 
 
 if __name__ == '__main__':
